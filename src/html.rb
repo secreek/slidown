20bef5de0075df695738a579054fd8fc705587c0
Void Main <voidmain1313113@gmail.com>
2012-10-30 22:44:13 +0800

    keyboard shortcut & remove some old .rb file

--- src/html.rb
+++
@@ -1,4 +0,0 @@
-require_relative 'renderer'
-
-class HtmlRenderer < Renderer
-end
--- src/sample.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'parser'
-require_relative 'builder'
-require_relative 'generator'
-
-mp = MarkdownParser.new(open('../docs/sample.md').read)
-b = Builder.new mp.parse
-g = Generator.new '../docs/'
-g.generate b.build_tree
