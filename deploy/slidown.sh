ab1e950a17c872f96f3915497b38573c45020730
Cliff Woo <cliffwoo@gmail.com>
2012-11-01 21:28:06 +0800

    fix some bugs

--- deploy/slidown.sh
+++ deploy/slidown.sh
@@ -9,20 +9,20 @@ THIN_CONFIG=/home/slidown/slidown.yml
 SOLIDOWN_CONFIG=/home/slidown/slidown/src/config.ru
 NOHUP=/usr/bin/nohup
 
-[-x "$DAEMON"] || exit 0
-
+[ -x "$THIN" ] || exit 0
+echo $1
 case "$1" in
 	start)
-		$NOHUP $THIN -C $THIN_CONFIG -R $SOLIDOWN_CONFIG start
+		$THIN -C $THIN_CONFIG -R $SOLIDOWN_CONFIG start
 		;;
 	stop)
-		$NOHUP $THIN -C $THIN_CONFIG -R $SOLIDOWN_CONFIG stop
+		$THIN -C $THIN_CONFIG -R $SOLIDOWN_CONFIG stop
 		;;
 	restart)
-		$NOHUP $THIN -C $THIN_CONFIG -R $SOLIDOWN_CONFIG restart
+		$THIN -C $THIN_CONFIG -R $SOLIDOWN_CONFIG restart
 		;;
 	*)
-		echo "Usage : $SCRIPT_NAME {start|stop|restart}" > &2
+		echo "Usage : $SCRIPT_NAME {start|stop|restart}" >&2
 		exit 3
 		;;
 esac
