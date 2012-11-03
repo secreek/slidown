8ee6dc1a3e7b6e7ea35587d2f5e91d7e9b9d1a2e
Void Main <voidmain1313113@gmail.com>
2012-11-03 11:00:58 +0800

    add service hook

--- src/server.rb
+++ src/server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
-require 'open-uri'
+require 'open-uri', 'base64', 'json'
+require 'net/http', 'uri'
 require_relative 'parser'
 require_relative 'builder'
 require_relative 'generator'
@@ -34,6 +35,39 @@ post '/:user/:topic/upload' do
   generator.generate builder.build_tree
 
   redirect "/#{@user}/#{@topic}"
+end
+
+post '/api/github_hooks'
+  push = JSON.parse(params[:payload])
+  @user = push['repository']['owner']['name']
+  @topic = push['repository']['name']
+  @path = "https://api.github.com/repos/#{@user}/#{@topic}/contents/show.md"
+
+  # Full control
+  uri = URI.parse("/api/#{user}/#{topic}/upload")
+  http = Net::HTTP.new(uri.host, uri.port)
+
+  request = Net::HTTP::Post.new(uri.request_uri)
+  request.set_form_data({"github_path" => @path})
+
+  http.request(request)
+end
+
+# try to provide an api for file upload
+post '/api/:user/:topic/upload'
+  @user = params[:user]
+  @topic = params[:topic]
+  @path = params[:github_path]
+
+  data = JSON.load(open(@path).read)
+  @file_content = Base64.decode64(data['content'])
+
+  parser = MarkdownParser.new(@file_content)
+  builder = Builder.new parser.parse
+  generator = Generator.new base_path, @user, @topic, 'guide'
+  generator.generate builder.build_tree
+
+  'Success!'
 end
 
 get '/:user/:topic/upload' do
