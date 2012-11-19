11ed83bdb25906d49767f149d3cb07334f0a128c
Cliff Woo <cliffwoo@gmail.com>
2012-11-19 15:43:44 +0800

    Add capistrano deploy script fix #8

---
+++ config/deploy.rb
@@ -0,0 +1,45 @@
+#========================
+#CONFIG
+#========================
+set :application, "slidown"
+ 
+set :scm, :git
+set :git_enable_submodules, 1
+set :repository, "git@github.com:secreek/slidown.git"
+set :branch, "master"
+set :ssh_options, { :forward_agent => true }
+ 
+set :stage, :production
+set :user, "slidown"
+set :use_sudo, false
+set :runner, "deploy"
+set :deploy_to, "/home/#{application}"
+set :app_server, :thin
+set :domain, "slidown.com"
+ 
+#========================
+#ROLES
+#========================
+role :app, domain
+role :web, domain
+role :db, domain, :primary => true
+
+#========================
+#CUSTOM
+#========================
+ 
+namespace :deploy do
+   # task :start, :roles => :app do
+   #  run "touch #{delpoy_to}/tmp/restart.txt"
+   # end
+ 
+  # task :stop, :roles => :app do
+  #   # Do nothing.
+  # end
+ 
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "ln -s /home/slidown/shared/file_repo /home/slidown/current/src/file_repo"
+    run "/home/slidown/slidown.sh restart"
+  end
+end
