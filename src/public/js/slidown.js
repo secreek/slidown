d64eead686f73418d9db1ab9e6d54275163af560
DeathKing <deathking0622@gmail.com>
2013-04-28 14:37:06 +0800

    这次冲突应该是合并完毕了。

--- src/public/js/slidown.js
+++ src/public/js/slidown.js
@@ -4,21 +4,13 @@ function nextPage() {
 
     first_child = get_meta('first_child');
     next_sib = get_meta('next_sib');
-<<<<<<< HEAD
-	next = get_meta('next');
-=======
     next = get_meta('next');
->>>>>>> 210135939bde3e2531edbcac9726221b601344fc
 
     // if(first_child != "") {
     //   base_path += first_child;
     // } else if(next_sib != "") {
     //   base_path += next_sib;
-<<<<<<< HEAD
-	if (next != "") {
-=======
 	if (next !== "") {
->>>>>>> 210135939bde3e2531edbcac9726221b601344fc
       base_path += next;
     } else {
       alert('The Show has come to an end');
@@ -27,29 +19,15 @@ function nextPage() {
     window.location = base_path;
 }
 
-<<<<<<< HEAD
-=======
 function prevPage () {
   history.go(-1);
 }
 
->>>>>>> 210135939bde3e2531edbcac9726221b601344fc
 document.onkeypress = function(e) {
   var e = window.event || e;
   if (e.charCode == 32) { // space is pressed
 	nextPage();
   }
-<<<<<<< HEAD
-}
-
-document.onclick = function(e) {
-	nextPage();
-}
-
-document.ontouchmove = function(e) {
-	nextPage();
-}
-=======
 };
 
 document.onclick = function(e) {
@@ -59,4 +37,3 @@ document.onclick = function(e) {
 document.ontouchmove = function(e) {
 	nextPage();
 };
->>>>>>> 210135939bde3e2531edbcac9726221b601344fc
