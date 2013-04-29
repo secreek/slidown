3bf4bdc1017a07b8516693bd40046f949784374b
DeathKing <deathking0622@gmail.com>
2013-04-29 12:26:46 +0800

    Meta parser rebuild.

--- src/meta.rb
+++ src/meta.rb
@@ -8,11 +8,10 @@ class MetaParser
 
     attr_reader     :proper
     REGEXPS = {
-        animation: /\s+animation:\s+(?<animation>\w+)\s+/,
-        style:     /\s+style:\s+(?<style>\w+)\s+/,
-        theme:     /\s+theme:\s+(?<theme>\w+)\s+/,
-        use:       /\s+use:\s+(?<plugin>\w+)\s+/,
-        meta:      /^<!--[\w|\d|\s]+-->/m,
+        animation: /\s*animation:\s*(?<animation>\w+)\s*/,
+        style:     /\s*style\s*:\s*(?<style>\w+)\s*/,
+        theme:     /\s*theme:\s*(?<theme>\w+)\s*/,
+        use:       /\s*use\s*:\s*(?<plugin>\w+)\s*/,
     }
 
     def initialize(str)
@@ -21,15 +20,23 @@ class MetaParser
             theme: "slide",
             use: [],
         }
-        parse_block(str.scan(REGEXPS[:meta]))
+        strblk = ""
+        str.each_line do |line|
+            strblk << line
+            break if line.strip.end_with? "-->"
+        end
+        strblk.gsub! "<!--",""
+        strblk.gsub! "-->",""
+        parse_block(strblk)
         self
     end
 
     def parse_block(string_block)
       return nil unless string_block.respond_to? :split
       string_block.split(/[;|\n|\r]/).each do |line|
-        line = line.chomp!.strip!
-        line.match(REGEXPS[:animation]) { |m| @porper[:animation] = m[1] }
+        line = line.chomp.strip
+        #puts line
+        line.match(REGEXPS[:animation]) { |m| @proper[:animation] = m[1] }
         line.match(REGEXPS[:theme]) { |m| @proper[:theme] = m[1] }
         line.match(REGEXPS[:style]) { |m| @proper[:style] = m[1] }
         line.match(REGEXPS[:use])   { |m| @proper[:use] << m[1] }
@@ -43,5 +50,10 @@ class MetaParser
     def method_missing(name, *arg)
         @proper[name.to_s.downcase.to_sym]
     end
+
+    def style;      @proper[:style];     end
+    def animation;  @proper[:animation]; end
+    def theme;      @proper[:theme];     end
+    def use;        @proper[:use];       end
 
 end
